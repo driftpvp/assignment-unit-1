4bb8d653ccbff157dd71e314f55b350739522663
Lucas Martin <driftpvp@gmail.com>
2023-06-14 00:35:08 -0500

    added header and photo

---
+++ assignment/helloWorld.js
@@ -0,0 +1,33 @@
+console.log("hello");
+
+let x = 1;
+
+let tempNow = 81;
+
+let tempTonight
+
+console.log("temp before too much gaming" , tempNow);
+
+tempNow = tempNow + 1;
+
+tempNow += 5;
+
+console.log(tempNow);
+let tempLater = '<p class="container">cool</p>'
+let game = "WoW";
+let title = "WotLK";
+// let year = "2023"
+game = game + ' ' + title;
+console.log('game' , game);
+
+let isCloudy = false
+
+let toReplace = document.querySelector('#Fun2');
+
+toReplace.innerHTML = tempLater;
+
+if (isCloudy === false) {
+    console.log('clear skys.');
+} else {
+    console.log('rainy day.');
+}
